6407cc08a7b4856a2b4ffc8466c25b7b9141b3a9
Josh Hoak <jrhoak@gmail.com>
2011-10-02 11:26:47 -0600

    Initial work on the post object

---
+++ src/post.py
@@ -0,0 +1,14 @@
+# TODO: add more copyright info
+# Licensed under the MIT License 
+
+class PostBuilder:
+  def __init__(self, post, date):
+    self.post = post 
+    self.date = date
+
+  def addTags(self, tags):
+    self.tags = tags
+
+  
+  
+    
