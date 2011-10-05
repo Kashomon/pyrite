e53e9b6bdc05b7628b3bf62bb1bdd7a15d2aa5e0
Alissa Pajer <alissa.pajer@gmail.com>
2011-10-04 20:36:01 -0600

    finished classes in post_property, added tests to test_post_properties

--- lib/tests/test_post_properties.py
+++ lib/tests/test_post_properties.py
@@ -4,25 +4,30 @@
 
 import unittest
 
+from datetime import datetime
+
 from .. import post_property 
 
 class TestPostProperties(unittest.TestCase):
 
     def test_title_compile(self):
-        title = post_property.Title("Post")
-        self.assertEqual(title.compile(), ("<div class=\"post_title\""+
-            "\">\nPost\n</div>"))
+        title = post_property.Title("Title")
+        self.assertEqual(title.compile(), "<div class=\"post_title\">\nTitle\n</div>")
 
     def test_post_body_compile(self):
-        pass
+        postbody = post_property.PostBody("Body")
+        self.assertEqual(postbody.compile(), "<div class=\"post_body\">\nBody\n</div>")
 
     def test_date_compile(self):
-        pass
-
+        now=datetime.now()
+        date = post_property.Date(now)
+        self.assertEqual(date.compile(), "<div class=\"post_date\">\n"+str(now)+"\n</div>")
+"""
     def test_tags_compile(self):
-        pass
+        tags = post_property.PostBody("Body")
+        self.assertEqual(postbody.compile(), "<div class=\"post_body\">\nBody\n</div>")        
+"""
 
 if __name__ == '__main__':
     unittest.main()
-
 
